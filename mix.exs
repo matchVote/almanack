89925ce897426eb2ca5e5888457da6cf4752b502
Barrett Helms <barrett.helms@salesloft.com>
2019-01-24 21:37:20 -0500

    Starts version over

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Almanack.MixProject do
   def project do
     [
       app: :almanack,
-      version: "0.1.1",
+      version: "0.1.0",
       elixir: "~> 1.8.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
