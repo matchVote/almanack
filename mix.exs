78957b0879438739108930ba3dafa608d4c0784c
Barrett Helms <barrett.helms@salesloft.com>
2019-02-10 14:53:01 -0500

    Bumps version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Almanack.MixProject do
   def project do
     [
       app: :almanack,
-      version: "0.2.2",
+      version: "0.3.0",
       elixir: "~> 1.8.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
